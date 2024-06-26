8710dbcabd54062212e7673dc58be83056ccff97
Lenovo <samy.elmenshawy96@gmail.com>
2024-06-26 14:24:09 +0300

    - make the structure of project - make the initial code of home

---
+++ lib/core/networking/api_service.dart
@@ -0,0 +1,21 @@
+import 'package:bulk_app/core/networking/api_constants.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
+
+
+part 'api_service.g.dart';
+
+@RestApi(baseUrl: ApiConstants.apiBaseUrl)
+abstract class ApiService {
+  factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+
+  // @POST(ApiConstants.login)
+  // Future<LoginResponse> login(
+  //   @Body() LoginRequestBody loginRequestBody,
+  // );
+
+  // @POST(ApiConstants.signup)
+  // Future<SignupResponse> signup(
+  //   @Body() SignupRequestBody signupRequestBody,
+  // );
+}
---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,44 @@
+import 'package:bulk_app/core/routing/routes.dart';
+import 'package:bulk_app/features/home/ui/pages/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+
+import '../../features/home/logic/cubit/home_cubit.dart';
+
+class AppRouter {
+  Route? generateRoute(RouteSettings settings) {
+    //this arguments to be passed in any screen like this ( arguments as ClassName )
+    final arguments = settings.arguments;
+
+    switch (settings.name) {
+      // case Routes.onBoardingScreen:
+      //   return MaterialPageRoute(
+      //     builder: (_) => const OnboardingScreen(),
+      //   );
+      // case Routes.loginScreen:
+      //   return MaterialPageRoute(
+      //     builder: (_) => BlocProvider(
+      //       create: (context) => getIt<LoginCubit>(),
+      //       child: const LoginScreen(),
+      //     ),
+      //   );
+      // case Routes.signUpScreen:
+      //   return MaterialPageRoute(
+      //     builder: (_) => BlocProvider(
+      //       create: (context) => getIt<SignupCubit>(),
+      //       child: const SignupScreen(),
+      //     ),
+      //   );
+      case Routes.homeScreen:
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => HomeCubit(),
+            child: const HomeScreen(),
+          ),
+        );
+      default:
+        return null;
+    }
+  }
+}
